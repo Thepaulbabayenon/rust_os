40118e9cdc5d3e2d91c06b23f49fb5a910f71fc8
PAUL BABAYEN-ON <138440106+Thepaulbabayenon@users.noreply.github.com>
2025-08-10 16:04:32 +0800

    learning rust

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
-#![no_std]
-#![no_main]
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
 
+mod vga_buffer;
 
 use core::panic::PanicInfo;
 
 
+/// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+
     loop {}
 }
